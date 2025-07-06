2fdb0baffedc57cb47c48a56597a2e96f4dee469
Aliba228 <121639272+Aliba228@users.noreply.github.com>
2025-07-07 01:03:33 +0500

    Update main.js

--- main.js
+++ main.js
@@ -1,19 +1,1 @@
-async function searchTracks(query) {
-  const response = await fetch(`https://soundgasm-api.vercel.app/search/${encodeURIComponent(query)}`);
-  const data = await response.json();
-  return data.results.map(r => ({
-    title: r.title,
-    artist: r.username,
-    url: r.audio_url,
-    artwork: "https://soundgasm.net/favicon.ico"
-  }));
-}
 
-async function play(url) {
-  return {
-    streamUrl: url,
-    mimeType: "audio/mpeg"
-  };
-}
-
-export default { searchTracks, play };
